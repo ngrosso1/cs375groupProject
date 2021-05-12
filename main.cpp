510088bb19f2157fa53807621fd53b398fd5f396
nick <ngrosso1@binghamton.edu>
2021-05-11 22:29:37 -0400

    rough draft of model implimentation

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
+
+using namespace std;
+
+void addEdge(vector<int> adj[], int x, int y){
+  adj[x].push_back(y);
+  adj[y].push_back(x);
+}
 
 int main() {
-    
+    int N; //Number of nodes
+    float P; //Prob of nodes
+    vector<int> g[N]; //adj list
+    srand(time(0)); //so rand() actually creates a random number
+
+    /* start adding nodes to the vector */
+
+    //Add edges to the graph randomly
+    for(int i = 0; i < N; i++){
+      for(int j = 0; j < N; j++){
+        if(i < j){
+          int R = rand(); //take a random number to compare P
+          if(R < P) addEdge(g, i, j);
+        }
+      }
+    }
+
     return 0;
 }
